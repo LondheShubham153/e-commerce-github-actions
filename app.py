06801cb643a4629657dedf5d19837a166651ecb8
LondheShubham153 <shubhamnath5@gmail.com>
2026-09-13 20:39:29 +0530

    added gitleaks alternative

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+DB_USER = "admin"
+DB_PASSWORD = "SuperSecretPassword123!" 
+
+def connect_db():
+    print(f"Connecting to database as {DB_USER}...")
+    # Connection logic here
+    
 def login(app_secret):
     print("logging in with password",app_secret)
 
