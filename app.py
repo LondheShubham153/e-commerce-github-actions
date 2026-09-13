a4a7bbd14917439560b93ff37d7223e983634bd5
LondheShubham153 <shubhamnath5@gmail.com>
2026-09-13 20:28:02 +0530

    added exposed password

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def login(app_secret):
+    print("logging in with password",app_secret)
+
 
 @app.route('/')
 def home():
+    app_secret = "xhdasjd5524==ere"
+    login(app_secret)
     return render_template('index.html')
