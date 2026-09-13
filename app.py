68db9c42496784675c70e3847113799375260582
LondheShubham153 <shubhamnath5@gmail.com>
2026-09-13 20:44:40 +0530

    added secrets scan

--- app.py
+++ app.py
@@ -2,19 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-DB_USER = "admin"
-DB_PASSWORD = "SuperSecretPassword123!" 
-
-def connect_db():
-    print(f"Connecting to database as {DB_USER}...")
-    # Connection logic here
-    
-def login(app_secret):
-    print("logging in with password",app_secret)
-
 
 @app.route('/')
 def home():
-    API_KEY = "xhdasjd5524==ere"
-    login(API_KEY)
     return render_template('index.html')
